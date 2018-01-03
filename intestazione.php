1b53139b37eca2bacea4ce5406aa82788edfa554
Claudio Branciforte <cbr_5@hotmail.it>
2018-01-03 16:44:37 +0100

    Bug Fix accentate
    Serve il codice della &amp per poterla usare per fare le lettere
    accentate, non so perché non funzioni con il simbolo di maggiore e
    minore.

--- intestazione.php
+++ intestazione.php
@@ -7,7 +7,7 @@
 <meta name="keywords" content="Hockey,Prato,Ragusa,Field">
 <meta name="author" content="Claudio Branciforte">
 
-<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <link rel="stylesheet" href="css/w3.css">
